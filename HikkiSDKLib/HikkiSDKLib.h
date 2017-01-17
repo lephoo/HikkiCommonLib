126d65a7ca27261f66cb35dcb82f5837c5bad48f
liupingpin <liupingpin@mac-mini-lpp.sh.the9.com>
2017-01-17 14:44:51 +0800

    added sdklib class

---
+++ HikkiSDKLib/HikkiSDKLib.h
@@ -0,0 +1,14 @@
+//
+//  HikkiSDKLib.h
+//  HikkiCommonLib
+//
+//  Created by liupingpin on 2017/1/17.
+//  Copyright © 2017年 Hikki. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SDKInterface.h"
+
+@interface HikkiSDKLib : NSObject<SDKInterface>
+
+@end
---
+++ HikkiSDKLib/HikkiSDKLib.m
@@ -0,0 +1,13 @@
+//
+//  HikkiSDKLib.m
+//  HikkiCommonLib
+//
+//  Created by liupingpin on 2017/1/17.
+//  Copyright © 2017年 Hikki. All rights reserved.
+//
+
+#import "HikkiSDKLib.h"
+
+@implementation HikkiSDKLib
+
+@end
